c4ad722fe2a2b469abc7ce24296ea367c597f630
mousa1995 <mousasoccerlove@gmail.com>
2022-04-20 17:01:51 +0430

    filling response information

--- src/Components/ipinfo.jsx
+++ src/Components/ipinfo.jsx
@@ -40,14 +40,14 @@ export default class IpInfo extends Component {
             {!this.state.isLoading &&
                 <div className="card-container">
                     <span className="info">Info</span>
-                    <img className="round" src="./br.svg" alt="brazil" />
-                    <h3>Brazil</h3>
-                    <h6>Guarulhos</h6>
-                    <p>isp: Akamai International B.V.</p>
+                    <img className="round" src={this.state.info.country_flag} alt={this.state.info.country} />
+                    <h3>{this.state.info.country}</h3>
+                    <h6>{this.state.info.city}</h6>
+                    <p>isp: {this.state.info.isp}</p>
                     <div className="footer">
-                        <p>IP: 2.21.90.0</p>
-                        <p>latitude: -23.4543395</p>
-                        <p>longitude: -46.5336678</p>
+                        <p>IP: {this.state.info.ip}</p>
+                        <p>latitude: {this.state.info.latitude}</p>
+                        <p>longitude: {this.state.info.longitude}</p>
                     </div>
                 </div>}
         </>
